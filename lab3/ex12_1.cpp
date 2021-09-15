2ca875f4279c282d17fdd7f8beba6ec777a4113c
luiselmuller <luiselmuller@gmail.com>
2021-09-15 15:35:01 -0400

    added lab3-part1
    part 1 of lab 3 done and divided the labs into folders

---
+++ lab3/ex12_1.cpp
@@ -0,0 +1,47 @@
+/*---------------------------------------
+|Luisel A. Muller Rodriguez 
+|#S01394043
+|This program takes an input of an
+|areas value and the units, divides it
+|by 2 and by 4 and displays both results
+|--------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+class Area
+{
+    public:
+    Area();
+    void display();
+
+    private:
+    string units;
+    float area_value;
+};
+
+int main ()
+{
+    Area *area1;
+
+    area1 = new Area; // Variables created using the new operator are called dynamic variables
+    area1 -> display();
+
+    delete area1; // Delete the dynamic variable p1 and return the memory occupied by p1 to the freestore to be reused.
+
+   return 0;
+}
+
+Area::Area()
+{
+    cout<<"Input the value of an area: ";
+    cin>>area_value;
+    cout<<"\nInput the units: ";
+    cin>>units;
+}
+
+void Area::display()
+{
+    cout<<"One-half of the area: "<<area_value/2<<units<<endl;
+    cout<<"One-quarter of the area: "<<area_value/4<<units<<endl;
+}
