7dcf944c0c71e8f244d4d1e3d622487267124f7e
luiselmuller <luiselmuller@gmail.com>
2021-09-16 13:52:40 -0400

    finished lab
    it works

--- lab3/ex12_2.cpp
+++ lab3/ex12_2.cpp
@@ -1,7 +1,10 @@
 /*---------------------------------------
 |Luisel A. Muller Rodriguez 
 |#S01394043
-|
+|This program makes a student "database"
+|where the person can input the student
+|name and id one by one and then view all
+|of the data in the database.
 |--------------------------------------*/
 
 #include <iostream>
@@ -12,22 +15,27 @@ class Student
 {
     public:
     Student();
-    void display(Student array);
+    void display();
 
     private:
     string student_name, studentid_number;
-
+    
 };
 
 int main()
 {
+    Student *sArr;
+
     int n;
-    cout<<"Enter the number of students in the database: ";
+    cout<<"Enter the number of students: ";
     cin>>n;
 
-    Student *sArr;
     sArr = new Student[n];
 
+    for(int i = 0; i < n; i++)
+    {
+        sArr[i].display();
+    }
 
     delete [] sArr;
 
@@ -35,10 +43,14 @@ int main()
 
 Student::Student()
 {
-    cout<<"Enter the name for the student: ";
+    cout<<"Enter a student name: ";
     cin>>student_name;
-
-    cout<<"Enter the id for the student: ";
+    cout<<"Enter a student id: ";
     cin>>studentid_number;
+    cout<<"\n";
 }
 
+void Student::display()
+{
+    cout<<"Student name -> "<<student_name<<" | Student ID -> "<<studentid_number<<"\n";
+}
