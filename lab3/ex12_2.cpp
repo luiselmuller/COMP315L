3a78bc8bcb248a6e87743bbe6633b2dca8da2372
luiselmuller <luiselmuller@gmail.com>
2021-09-20 15:17:04 -0400

    completed code

--- lab3/ex12_2.cpp
+++ lab3/ex12_2.cpp
@@ -8,37 +8,39 @@
 |--------------------------------------*/
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 class Student
 {
-    public:
+public:
     Student();
     void display();
 
-    private:
+private:
     string student_name, studentid_number;
 };
 
 int main()
 {
-    Student *sArr;
+    Student* sArr;
 
     //variable used to determine the size of the array
     int n;
-    cout<<"Enter the number of students: ";
-    cin>>n;
+    cout << "Enter the number of students: ";
+    cin >> n;
 
     sArr = new Student[n];//dynamic array with n amount of students
 
     //used to iterate through the dynamic array and display each object in it
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         sArr[i].display();
     }
-    
+
     //deletes the block of allocated memory for sArr
-    delete [] sArr;
+    delete[] sArr;
 
     return 0;
 }
@@ -47,18 +49,18 @@ int main()
 Student::Student()
 {
     cin.ignore();//removes extra characters left after using cin
-    cout<<"Enter a student name: ";
+    cout << "Enter a student name: ";
     getline(cin, student_name);
-    cout<<"Enter a student id: ";
+    cout << "Enter a student id: ";
     getline(cin, studentid_number);
-    cout<<"Press enter to continue...";
-    cout<<"\n";
+    cout << "Press enter to continue...";
+    cout << "\n";
 }
 
 //function used to display the variable values
 void Student::display()
 {
-    cout<<"+------------------------------------------------+\n";
-    cout<<" Student Name | "<<student_name<<" \n";
-    cout<<" Student ID   | "<<studentid_number<<" \n";
+    cout << "+-------------------------------------------------------------------------------+\n";
+    cout << " Student Name | " << student_name << " \n";
+    cout << " Student ID   | " << studentid_number << " \n";
 }
